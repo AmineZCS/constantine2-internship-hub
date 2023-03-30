3330569ec059202265150500a802d3b41975f99c
AmineZCS <medamineben19@gmail.com>
2023-03-30 02:30:08 +0100

    adding migration files

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// login and return token
+
+Route::post('/sanctum/token', function (Request $request) {
+    // $request->validate([
+    //     'email' => 'required|email',
+    //     'password' => 'required',
+    //     'device_name' => 'required',
+    // ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+     return $user->createToken($request->email)->plainTextToken;
+
+});
