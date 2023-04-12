e19d112c4ba0a4c72806d42c6546bc018195de7e
AmineZCS <medamineben19@gmail.com>
2023-04-12 09:13:55 +0100

    created InternshipDepartment model

---
+++ app/Models/InternshipDepartment.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InternshipDepartment extends Model
+{
+    use HasFactory;
+    protected $table = 'internship_department';
+    protected $guarded = [];
+
+}
